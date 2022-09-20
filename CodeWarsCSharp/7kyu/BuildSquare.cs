11c70fa59de328f54a88c533047ed85d3511223e
TIS-NB-014\Seth Del Castillo <seth.delcastillo@tisconsulting.org>
2022-09-20 11:27:04 -0700

    nuevo kata

--- CodeWarsCSharp/7kyu/BuildSquare.cs
+++ CodeWarsCSharp/7kyu/BuildSquare.cs
@@ -1,16 +1,22 @@
 ﻿//I will give you an integer. Give me back a shape that is as long and wide as the integer. The integer will be a whole number between 1 and 50.
-var n = Console.ReadLine();
-string tab = "";
-for (int i = 0; i < Convert.ToInt32(n); i++)
-{
-    for (int j = 0; j < Convert.ToInt32(n); j++)
-    {
-        tab += "+";
-    }
-    if (i < (Convert.ToInt32(n) - 1))
-    {
-        tab += "\n";
-    }
 
-}
-Console.WriteLine(tab);
+//var list = new List<object>() { 1, 2, "aasf", "1", "123", 123 };
+//var a = list.Where(x => x.GetType() == typeof(int)).Cast<int>();
+
+//Console.WriteLine(a);
+
+//var n = Console.ReadLine();
+//string tab = "";
+//for (int i = 0; i < Convert.ToInt32(n); i++) = 
+//{
+//    for (int j = 0; j < Convert.ToInt32(n); j++)
+//    {
+//        tab += "+";
+//    }
+//    if (i < (Convert.ToInt32(n) - 1))
+//    {
+//        tab += "\n";
+//    }
+
+//}
+//Console.WriteLine(tab);
---
+++ CodeWarsCSharp/7kyu/ListFiltering.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsCSharp._7kyu
+{
+    internal class ListFiltering
+    {
+        public void GetIntegersFromList(List<object> listOfItems)
+        {
+            var list = new List<object>() { 1, 2, "aasf", "1", "123", 123 };
+            var a = list.Where(x => x.GetType() == typeof(int)).Cast<int>();
+
+            Console.WriteLine(a);
+        }
+    }
+}
