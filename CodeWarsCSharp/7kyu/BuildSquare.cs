7ca129ee3f154d4bb54d29219ac78cea0b9cbb11
TIS-NB-014\Seth Del Castillo <seth.delcastillo@tisconsulting.org>
2022-09-20 10:33:57 -0700

    nuevo kyu

---
+++ CodeWarsCSharp/7kyu/BuildSquare.cs
@@ -0,0 +1,16 @@
+﻿//I will give you an integer. Give me back a shape that is as long and wide as the integer. The integer will be a whole number between 1 and 50.
+var n = Console.ReadLine();
+string tab = "";
+for (int i = 0; i < Convert.ToInt32(n); i++)
+{
+    for (int j = 0; j < Convert.ToInt32(n); j++)
+    {
+        tab += "+";
+    }
+    if (i < (Convert.ToInt32(n) - 1))
+    {
+        tab += "\n";
+    }
+
+}
+Console.WriteLine(tab);
